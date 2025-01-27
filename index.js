080dcec21ed15bbf758f59ff97cf0e8bc4ed0e38
MasumAhmed19 <masum.ahmed1328@gmail.com>
2025-01-28 02:50:43 +0600

    add api: all task with name, month, year query

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const jwt = require('jsonwebtoken')
-const morgan = require('morgan')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const morgan = require("morgan");
 
-const port = process.env.PORT || 9000
-const app = express()
+const port = process.env.PORT || 9000;
+const app = express();
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
-}
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
+  const token = req.cookies?.token;
 
   if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
+    return res.status(401).send({ message: "unauthorized access" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
+      console.log(err);
+      return res.status(401).send({ message: "unauthorized access" });
     }
-    req.user = decoded
-    next()
-  })
-}
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f0l8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -46,44 +46,43 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-})
-
+});
 
 async function run() {
   try {
     const db = client.db("TeamLoom-db");
     const employeeCollection = db.collection("employee");
-    const taskCollection = db.collection("tasks")
-    const salaryCollection = db.collection("payroll")
+    const taskCollection = db.collection("tasks");
+    const salaryCollection = db.collection("payroll");
 
     // Generate jwt token
-    app.post('/jwt', async (req, res) => {
-      const email = req.body
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
       const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '365d',
-      })
+        expiresIn: "365d",
+      });
       res
-        .cookie('token', token, {
+        .cookie("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
-        .send({ success: true })
-    })
+        .send({ success: true });
+    });
     // Logout
-    app.get('/logout', async (req, res) => {
+    app.get("/logout", async (req, res) => {
       try {
         res
-          .clearCookie('token', {
+          .clearCookie("token", {
             maxAge: 0,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
           })
-          .send({ success: true })
+          .send({ success: true });
       } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send(err);
       }
-    })
+    });
 
     // POST ALL USER DATA
     // app.post('/add-user', async(req, res)=>{
@@ -96,244 +95,256 @@ async function run() {
     // })
 
     // POST ALL USER DATA
-    app.post('/add-user/:email', async(req, res)=>{
+    app.post("/add-user/:email", async (req, res) => {
       const email = req.params.email;
       const userData = req.body;
-      
+
       // check if user exists in db
-      const query = {email};
-      const isExist = await employeeCollection.findOne(query)
+      const query = { email };
+      const isExist = await employeeCollection.findOne(query);
 
       // jodi age theke user theke thake
-      if(isExist){ 
-        return res.send(isExist)
+      if (isExist) {
+        return res.send(isExist);
       }
 
       // jodi na thake
       const result = await employeeCollection.insertOne({
         ...userData,
-        role:userData?.role || 'employee',
+        role: userData?.role || "employee",
         timestamp: Date.now(),
       });
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // GET ALL USER DATA for admin
-    app.get('/all-employee', async(req, res)=>{
-      const role = req.query.role || ''
-      const query = role ? {role:role} : {}
-      const result = await employeeCollection.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/all-employee", async (req, res) => {
+      const role = req.query.role || "";
+      const query = role ? { role: role } : {};
+      const result = await employeeCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Get employee detail through their email
-    app.get('/employee/:email', async(req, res)=>{
-      const email = req.params.email
-      const query = {'email':email}
-      const result = await employeeCollection.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/employee/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await employeeCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Get employee detail through their id
-    app.get('/employee/id/:id', async(req, res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await employeeCollection.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/employee/id/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await employeeCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Update employee detail through their id
-    app.put('/makehr/:id', async(req, res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)};
+    app.put("/makehr/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const updateData = {
         makeHR: true,
-        role: 'hr'
-      }
+        role: "hr",
+      };
       const update = {
-        $set:updateData
-      }
+        $set: updateData,
+      };
       const options = {
-        upsert: false
-      } 
-      const result = await employeeCollection.updateOne(query, update, options)
+        upsert: false,
+      };
+      const result = await employeeCollection.updateOne(query, update, options);
 
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // update salary
-    app.put('/adjust-salary/:id', async(req, res)=>{
-      const id= req.params.id;
-      const {salary}= req.body
-      const query = {_id: new ObjectId(id)};
+    app.put("/adjust-salary/:id", async (req, res) => {
+      const id = req.params.id;
+      const { salary } = req.body;
+      const query = { _id: new ObjectId(id) };
       const updatedData = {
-        salary: parseInt(salary)
-      }
+        salary: parseInt(salary),
+      };
       const update = {
-        $set:updatedData
-      }
+        $set: updatedData,
+      };
 
       const options = {
-        upsert: false
-      }
-      const result = await employeeCollection.updateOne(query, update, options)
+        upsert: false,
+      };
+      const result = await employeeCollection.updateOne(query, update, options);
 
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/role/:email', async (req, res)=>{
+    app.get("/role/:email", async (req, res) => {
       const email = req.params.email;
-      const query = {email:email}
-      const result = await employeeCollection.findOne(query)
+      const query = { email: email };
+      const result = await employeeCollection.findOne(query);
       res.send({ role: result.role });
-    })
+    });
 
-    // delete/Fire from job 
-    app.delete('/fire/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await employeeCollection.deleteOne(query)
-      res.send(result)
-    })
+    // delete/Fire from job
+    app.delete("/fire/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await employeeCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // API FOR HR---> TODO: hr middleware
 
     // Getting All Employee
-    app.get('/only-employees', async (req, res)=>{
-        const query = {role: 'employee'}
-        const result = await employeeCollection.find(query).toArray()
-        res.send(result)
-    })
+    app.get("/only-employees", async (req, res) => {
+      const query = { role: "employee" };
+      const result = await employeeCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // isVerify value toggling
-    app.patch('/verify/:id', async (req, res)=>{
-      const id= req.params;
-      const query = {_id: new ObjectId(id)};
+    app.patch("/verify/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: new ObjectId(id) };
       const user = await employeeCollection.findOne(query);
 
       const update = {
         $set: {
-          isVerified: !user.isVerified, 
+          isVerified: !user.isVerified,
         },
       };
 
       const result = await employeeCollection.updateOne(query, update);
 
-      res.send(result)
-
-    })
+      res.send(result);
+    });
 
     // payment request
-    app.post('/payment-req', async (req, res)=>{
-      const data = req.body
-      try{
-        const {month, year, employee_id} = data;
+    app.post("/payment-req", async (req, res) => {
+      const data = req.body;
+      try {
+        const { month, year, employee_id } = data;
 
         const existingPayment = await salaryCollection.findOne({
-          employee_id:employee_id,
+          employee_id: employee_id,
           month: month,
-          year: year
-        })
-        
-        if(existingPayment){
+          year: year,
+        });
+
+        if (existingPayment) {
           return res.status(400).json({
-            message:"Payment Exists"
-          })
+            message: "Payment Exists",
+          });
         }
 
         const result = await salaryCollection.insertOne(data);
         res.send(result);
-      }catch(err){
+      } catch (err) {
         res.status(500).json({
-          message:"An error occurred",
-          error: err.message
-        })
+          message: "An error occurred",
+          error: err.message,
+        });
       }
-
-    })
+    });
 
     // get all payroll listings
-    app.get('/all-payment-request', async (req,res)=>{
+    app.get("/all-payment-request", async (req, res) => {
       const result = await salaryCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
+    // get all tasks for progress in HR
+    app.get("/all-tasks", async (req, res) => {
+      const { name, month, year } = req.query;
+      const filter = {};
 
+      if (name) {
+        filter.employeeName = { $regex: name, $options: "i" };
+      }
 
+      if (month) {
+        // Ensure the month is correctly matched as the second part of the date
+        filter.date = {
+          $regex: `^[0-9]{2}-${month.padStart(2, "0")}-`,
+          $options: "i",
+        };
+      }
 
-    app.post('/add-task', async(req, res)=>{
+      // Filter by year
+      if (year) {
+        filter.date = { ...filter.date, $regex: `${year}` };
+      }
+
+      const result = await taskCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    // get all tasks
+    app.post("/add-task", async (req, res) => {
       const data = req.body;
       const result = await taskCollection.insertOne(data);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/tasks/:email', async(req, res)=>{
-      const email= req.params.email
-      const query = {'employeeEmail': email}
+    app.get("/tasks/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { employeeEmail: email };
       const result = await taskCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/tasks/:email', async(req, res)=>{
-      const email= req.params.email
-      const query = {'employeeEmail': email}
-      const result = await taskCollection.find(query).toArray();
-      res.send(result)
-    })
-
-    app.put('/update-task/:id', async(req, res)=>{
+    app.put("/update-task/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const { updatedTask, updatedHours, newDate } = req.body;
 
       const update = {
-        $set:{
+        $set: {
           task: updatedTask,
           hoursWorked: updatedHours,
-          date: newDate
-        }
-      }
+          date: newDate,
+        },
+      };
 
       const options = {
-        upsert:false
-      }
-      const result = await taskCollection.updateOne(query, update, options)
+        upsert: false,
+      };
+      const result = await taskCollection.updateOne(query, update, options);
 
-      res.send(result)
-  
+      res.send(result);
+    });
 
-    })
-
-
-    app.delete('/delete-task/:id', async(req, res)=>{
+    app.delete("/delete-task/:id", async (req, res) => {
       const id = req.params;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await taskCollection.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-
-    app.get('/employee/role/:email', async (req, res)=>{
+    // API for role
+    app.get("/employee/role/:email", async (req, res) => {
       const email = req.params.email;
-      const result = await employeeCollection.findOne({email});
-      res.send({role: result?.role})
-    })
+      const result = await employeeCollection.findOne({ email });
+      res.send({ role: result?.role });
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello from TeamLoom Server');
-})
+app.get("/", (req, res) => {
+  res.send("Hello from TeamLoom Server");
+});
 
 app.listen(port, () => {
-  console.log(`TeamLoom is running on port ${port}`)
-})
+  console.log(`TeamLoom is running on port ${port}`);
+});
